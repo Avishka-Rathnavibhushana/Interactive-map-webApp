eac5cf9529749826e6cfac9926a7bd1b667836bb
Avishka-Rathnavibushana <mailavishka@gmail.com>
2022-09-29 03:01:05 +0530

    Remove support for IOS/Safari

--- interactive-map/lib/main.dart
+++ interactive-map/lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:interactive_map/constants/constants.dart';
 import 'package:interactive_map/controller/appBind.dart';
 import 'package:interactive_map/screens/home.dart';
@@ -9,6 +11,8 @@ import 'package:interactive_map/screens/vechicles/excavator.dart';
 import 'package:interactive_map/screens/vechicles/sportsCar.dart';
 import 'package:interactive_map/screens/vechicles/train.dart';
 import 'package:interactive_map/screens/vechicles/vechicles_home.dart';
+import 'package:web_browser_detect/web_browser_detect.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,6 +30,10 @@ class _InteractiveMapState extends State<InteractiveMap> {
   @override
   void initState() {
     super.initState();
+    if (kIsWeb) {
+      print(kIsWeb);
+      // running on the web!
+    }
   }
 
   @override
@@ -35,7 +43,10 @@ class _InteractiveMapState extends State<InteractiveMap> {
       title: 'Turntide App',
       initialBinding: AppBinding(),
       scrollBehavior: MyCustomScrollBehavior(),
-      home: const Home(),
+      home: Theme.of(context).platform == TargetPlatform.iOS ||
+              Browser().browser == 'Safari'
+          ? EmptyScreen()
+          : Home(),
     );
   }
 }
@@ -47,4 +58,29 @@ class MyCustomScrollBehavior extends MaterialScrollBehavior {
         PointerDeviceKind.touch,
         PointerDeviceKind.mouse,
       };
+}
+
+class EmptyScreen extends StatelessWidget {
+  const EmptyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'This app is currently unavailable on iOS/Safari, for optimal experience please use chrome on a computer or android mobile device.',
+          style: GoogleFonts.barlow(
+            textStyle: TextStyle(
+              color: AppColors.night,
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+              letterSpacing: 2,
+            ),
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
 }
